904ef0c8a7dab24133466c9c08e973cfa84e7f9b
Rishav-Kaushal <rishavkaushal256@gmail.com>
2026-01-14 11:28:53 +0530

    added day 3 tuple assign

--- assignments/#Day3-tuple.py
+++ assignments/#Day3-tuple.py
@@ -20,7 +20,7 @@ print(tuple1[2:6])
 tpl1 = (1,2,3)
 tpl2 = (4,5,6)
 totpl = tpl1+tpl2
-print(totpl)
+print("concatationof two tuple : ",totpl)
 
 #6.
 dup_tpl = (1,2,31,26,3,1,3)
@@ -34,9 +34,59 @@ a,b,c,d,e = tpl11
 print(a,b,c,d,e)
 
 #8.
-list=[1,2,3,4,5]
-tpl=tuple(list)
-print(tpl)
+list1=[1,2,3,4,5]
+tpl = tuple(list1)
+print("list to tuple",tpl)
 
 #9.
-tploftpl=tuple(tuple())
+tploftpl=(
+    (1,2,3),
+    (4,5,6),
+    (7,8,9)
+)
+print("Tuple of tuple:",tploftpl)
+
+#10.
+t = tuple(x for x in range(1,6))
+l = list(t)
+l.append(6)
+tu = tuple(l)
+print("Resulting tuple:",tu)
+
+#11.
+tup=("rk","sk","vb")
+str=""
+for i in tup:
+    str+=i
+print(str)
+print(type(str))
+
+#12.
+dict={
+    
+}
+
+#13.
+nested_tpl=(
+    (1,2,3),
+    (4,5,6),
+    (7,8,9)
+)
+for sub in nested_tpl:
+    for item in sub:
+        print(item,end=" ")
+print()
+
+#14.
+tuple11=(1,2,3,6,3,5,2,8,5,6)
+set=set(tuple11)
+tuple12=tuple(set)
+print(tuple11)
+
+print(tuple12)
+
+#15.
+lst1=list()
+for i in range(6):
+    lst1.append(int(input("Ënter the numbers:"))) #append use not index
+print("the tuple by user input:",tuple(lst1))
