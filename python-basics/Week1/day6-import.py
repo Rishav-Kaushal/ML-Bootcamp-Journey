4d22703bbcd3dfc750031ad70bac24bc7336a996
Rishav-Kaushal <rishavkaushal256@gmail.com>
2026-01-23 22:48:29 +0530

    added

--- python-basics/Week1/day6-import.py
+++ python-basics/Week1/day6-import.py
@@ -1,1 +1,17 @@
 #Day6---------Importing_Modules & Packages------------------------------------------
+
+
+import math
+# mathmatically calcu package
+print(math.sqrt(40))
+
+from math  import sqrt,pi
+#from <package name> import <inside the package>
+print(sqrt(16))
+print(pi)
+
+import numpy as np
+#np is alias means short name for using multiple type
+
+
+
